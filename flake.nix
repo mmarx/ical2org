81861c382ede20a28e07c648294f01d2a30053e9
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2024-05-31 22:15:53 +0200

    Update to nixpkgs-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "basic rust template";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -13,24 +13,24 @@
     };
   };
 
-  outputs = { self, utils, ... }@inputs:
+  outputs =
+    { self, utils, ... }@inputs:
     utils.lib.mkFlake {
       inherit self inputs;
-      channels.nixpkgs.overlaysBuilder = channels:
-        [ inputs.rust-overlay.overlays.rust-overlay ];
+      channels.nixpkgs.overlaysBuilder = channels: [ inputs.rust-overlay.overlays.rust-overlay ];
 
-      outputsBuilder = channels:
+      outputsBuilder =
+        channels:
         let
           pkgs = channels.nixpkgs;
-          toolchain =
-            pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           platform = pkgs.makeRustPlatform {
             cargo = toolchain;
             rustc = toolchain;
           };
-          cargoMeta =
-            (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
-        in rec {
+          cargoMeta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+        in
+        rec {
           packages = rec {
             ical2org = platform.buildRustPackage {
               pname = "ical2org";
@@ -39,8 +39,11 @@
               src = ./.;
               cargoLock.lockFile = ./Cargo.lock;
 
-              nativeBuildInputs =
-                [ toolchain platform.cargoBuildHook platform.cargoCheckHook ];
+              nativeBuildInputs = [
+                toolchain
+                platform.cargoBuildHook
+                platform.cargoCheckHook
+              ];
 
               meta = {
                 inherit (cargoMeta) description homepage;
@@ -50,53 +53,62 @@
             default = ical2org;
           };
 
-          checks = let
-            runCargo' = name: env: buildCommand:
-              pkgs.stdenv.mkDerivation ({
-                preferLocalBuild = true;
-                allowSubstitutes = false;
+          checks =
+            let
+              runCargo' =
+                name: env: buildCommand:
+                pkgs.stdenv.mkDerivation (
+                  {
+                    preferLocalBuild = true;
+                    allowSubstitutes = false;
 
-                RUSTFLAGS = "-Dwarnings";
-                RUSTDOCFLAGS = "-Dwarnings";
+                    RUSTFLAGS = "-Dwarnings";
+                    RUSTDOCFLAGS = "-Dwarnings";
 
-                src = ./.;
-                cargoDeps =
-                  platform.importCargoLock { lockFile = ./Cargo.lock; };
+                    src = ./.;
+                    cargoDeps = platform.importCargoLock { lockFile = ./Cargo.lock; };
 
-                nativeBuildInputs = [ toolchain ]
-                  ++ (with platform; [ cargoSetupHook pkgs.python3 ]);
+                    nativeBuildInputs =
+                      [ toolchain ]
+                      ++ (with platform; [
+                        cargoSetupHook
+                        pkgs.python3
+                      ]);
 
-                inherit name;
+                    inherit name;
 
-                buildPhase = ''
-                  runHook preBuild
-                  mkdir $out
-                  ${buildCommand}
-                  runHook postBuild
-                '';
-              } // env);
-            runCargo = name: runCargo' name { };
-          in {
-            inherit (packages) ical2org;
+                    buildPhase = ''
+                      runHook preBuild
+                      mkdir $out
+                      ${buildCommand}
+                      runHook postBuild
+                    '';
+                  }
+                  // env
+                );
+              runCargo = name: runCargo' name { };
+            in
+            {
+              inherit (packages) ical2org;
 
-            devshell = devShells.default;
+              devshell = devShells.default;
 
-            clippy = runCargo "ical2org-check-clippy" ''
-              cargo clippy --all-targets
-            '';
+              clippy = runCargo "ical2org-check-clippy" ''
+                cargo clippy --all-targets
+              '';
 
-            doc = runCargo "ical2org-check-docs" ''
-              cargo doc --workspace
-            '';
+              doc = runCargo "ical2org-check-docs" ''
+                cargo doc --workspace
+              '';
 
-            fmt = runCargo "ical2org-check-formatting" ''
-              cargo fmt --all -- --check
-            '';
+              fmt = runCargo "ical2org-check-formatting" ''
+                cargo fmt --all -- --check
+              '';
 
-            test = runCargo "ical2org-check-tests" ''
-              cargo test
-            '';
-          };
+              test = runCargo "ical2org-check-tests" ''
+                cargo test
+              '';
+            };
 
           devShells.default = channels.nixpkgs.mkShell {
             RUST_LOG = "debug";
