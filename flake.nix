fb80c77f946de580e05779cc5dd588075e547269
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-12-07 16:23:45 +0100

    Drop rust-overlay

--- flake.nix
+++ flake.nix
@@ -2,45 +2,32 @@
   description = "ical2org - convert ical calenders into org agendas";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # rust-overlay = {
+    #   url = "github:oxalica/rust-overlay";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs =
     { self, utils, ... }@inputs:
     utils.lib.mkFlake {
       inherit self inputs;
-      channels.nixpkgs.overlaysBuilder = channels: [ inputs.rust-overlay.overlays.rust-overlay ];
-
       outputsBuilder =
         channels:
         let
           pkgs = channels.nixpkgs;
-          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          platform = pkgs.makeRustPlatform {
-            cargo = toolchain;
-            rustc = toolchain;
-          };
-          cargoMeta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+          cargoMeta = (fromTOML (builtins.readFile ./Cargo.toml)).package;
         in
-        rec {
+        {
           packages = rec {
-            ical2org = platform.buildRustPackage {
+            ical2org = pkgs.rustPlatform.buildRustPackage {
               pname = "ical2org";
               inherit (cargoMeta) version;
 
               src = ./.;
               cargoLock.lockFile = ./Cargo.lock;
-
-              nativeBuildInputs = [
-                toolchain
-                platform.cargoBuildHook
-                platform.cargoCheckHook
-              ];
 
               meta = {
                 inherit (cargoMeta) description homepage;
@@ -50,73 +37,19 @@
             default = ical2org;
           };
 
-          checks =
-            let
-              runCargo' =
-                name: env: buildCommand:
-                pkgs.stdenv.mkDerivation (
-                  {
-                    preferLocalBuild = true;
-                    allowSubstitutes = false;
-
-                    RUSTFLAGS = "-Dwarnings";
-                    RUSTDOCFLAGS = "-Dwarnings";
-
-                    src = ./.;
-                    cargoDeps = platform.importCargoLock { lockFile = ./Cargo.lock; };
-
-                    nativeBuildInputs =
-                      [ toolchain ]
-                      ++ (with platform; [
-                        cargoSetupHook
-                        pkgs.python3
-                      ]);
-
-                    inherit name;
-
-                    buildPhase = ''
-                      runHook preBuild
-                      mkdir $out
-                      ${buildCommand}
-                      runHook postBuild
-                    '';
-                  }
-                  // env
-                );
-              runCargo = name: runCargo' name { };
-            in
-            {
-              inherit (packages) ical2org;
-
-              devshell = devShells.default;
-
-              clippy = runCargo "ical2org-check-clippy" ''
-                cargo clippy --all-targets
-              '';
-
-              doc = runCargo "ical2org-check-docs" ''
-                cargo doc --workspace
-              '';
-
-              fmt = runCargo "ical2org-check-formatting" ''
-                cargo fmt --all -- --check
-              '';
-
-              test = runCargo "ical2org-check-tests" ''
-                cargo test
-              '';
-            };
-
           devShells.default = channels.nixpkgs.mkShell {
             RUST_LOG = "debug";
             RUST_BACKTRACE = 1;
-            buildInputs = [
-              toolchain
-              pkgs.rust-analyzer
-              pkgs.cargo-audit
-              pkgs.cargo-license
-              pkgs.cargo-edit
-            ];
+            buildInputs = pkgs.lib.attrValues {
+              inherit (pkgs)
+                cargo
+                cargo-audit
+                cargo-edit
+                cargo-license
+                rustc
+                rust-analyzer
+                ;
+            };
           };
         };
     };
